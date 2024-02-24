70fed37af325e67670a47774c9eb4095b305998e
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2024-02-24 16:38:58 +0200

    Node ja npm versioita muutettu

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Tervetuloa!");
+    res.send("Tervetuloa takaisin!");
 });
 
 app.get('/test', (req, res) => {
